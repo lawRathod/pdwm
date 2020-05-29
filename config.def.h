ae874dcdce07cca0ec02519b2dc6478fe51d5f34
Prateek <emaillawrathod@gmail.com>
2020-05-29 16:39:51 +0530

    usable build of dwm

--- config.def.h
+++ config.def.h
@@ -1,12 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -59,13 +55,13 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-/* component of dmenucmd, manipulated in spawn() */
-static const char *launcher[] = { "rofi", "-show", "drun" };
-static const char *termcmd[]  = { "terminator", NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = launcher } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -116,4 +112,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
--- config.h
+++ config.h
@@ -9,17 +9,16 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "Google Sans:size=10" };
+static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#dfdfdf";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_gray3,  col_cyan  },
 };
 
 /* tagging */
@@ -33,6 +32,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "mpv",  	  NULL,       NULL,       0,       1,           -1 },
 };
 
 /* layout(s) */
@@ -48,7 +48,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define MODKEY1 Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -59,14 +60,39 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-/* component of dmenucmd, manipulated in spawn() */
-static const char *launcher[] = { "rofi", "-show", "drun" };
+static const char *launcher[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "terminator", NULL };
+static const char *screenshot[]  = { "/home/law/apps/scripts/screenshot.sh", NULL };
+static const char *screenshotsave[]  = { "/home/law/apps/scripts/screenshot.sh", "--save", NULL };
+static const char *audiomute[]  = {"pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *audioinc[]  = { "pactl", "set-sink-volume" ,"0", "+5%", NULL };
+static const char *audiodec[]  = { "pactl", "set-sink-volume" ,"0", "-5%", NULL };
+static const char *brightnessinc[]  = { "xbacklight", "-inc" ,"5", NULL };
+static const char *brightnessdec[]  = { "xbacklight", "-dec" ,"5", NULL };
+static const char *mpctoggle[]  = { "mpc", "toggle",NULL };
+static const char *mpcnext[]  = { "mpc", "next",NULL };
+static const char *mpcprev[]  = { "mpc", "prev",NULL };
+static const char *nemo[]  = { "nemo" ,NULL };
+static const char *suspend[]  = { "systemctl", "suspend" ,NULL };
+static const char *lock[]  = { "xsecurelock" ,NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = launcher } },
+	{ 0,                       		XK_Print,  spawn,		   {.v = screenshot } },
+	{ 0,             				XF86XK_AudioMute, spawn,   {.v = audiomute } },
+	{ 0,             				XF86XK_AudioRaiseVolume, spawn,          {.v = audioinc } },
+	{ 0,             				XF86XK_AudioLowerVolume, spawn,          {.v = audiodec } },
+	{ 0,             				XF86XK_MonBrightnessUp, spawn,          {.v = brightnessinc } },
+	{ 0,             				XF86XK_MonBrightnessDown, spawn,          {.v = brightnessdec } },
+	{ 0,             				XF86XK_AudioPlay, spawn,          {.v = mpctoggle } },
+	{ 0,             				XF86XK_AudioNext, spawn,          {.v = mpcnext } },
+	{ 0,             				XF86XK_AudioPrev, spawn,          {.v = mpcprev } },
+	{ MODKEY,                       XK_space,  spawn,		   {.v = launcher } },
+	{ MODKEY,                       XK_e,  spawn,		   {.v = nemo } },
+	{ MODKEY,                       XK_Print,  spawn,		   {.v = screenshotsave } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,             			XK_u, spawn,          {.v = lock } },
+	{ MODKEY|ShiftMask,             XK_u, spawn,          {.v = suspend } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -76,11 +102,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,             			XK_w,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
